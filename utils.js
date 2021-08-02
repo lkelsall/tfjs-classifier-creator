6a105d050a0e35f948621fd8b8b78686688e835c
PaulOR26 <pj_orourke@outlook.com>
2021-08-02 12:07:14 +0100

    create trainClassFromImages utils function

--- utils.js
+++ utils.js
@@ -1,10 +1,20 @@
-const { readFileSync } = require('fs');
+const { readFileSync, readdirSync } = require('fs');
 const tf = require('@tensorflow/tfjs-node');
 
-const tensorFromJpg = (filePath) => {
+exports.tensorFromJpg = (filePath) => {
   const imageData = readFileSync(filePath);
 
   const imgTensor = tf.node.decodeImage(new Uint8Array(imageData), 3);
 
   return imgTensor;
+};
+
+exports.trainClassFromImages = (classifier, filePath, model, className) => {
+  const imageFileNames = readdirSync(filePath);
+
+  imageFileNames.forEach((imageFile) => {
+    const imageTensor = this.tensorFromJpg(`${filePath}/${imageFile}`);
+    const logits = model.infer(imageTensor);
+    classifier.addExample(logits, className);
+  });
 };
